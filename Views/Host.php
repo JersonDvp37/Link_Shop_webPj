52a5b58eb6d5608b148333ed81a85e28903ddf13
JersonDvp37 <129074611+JersonDvp37@users.noreply.github.com>
2025-02-27 18:57:52 +0000

    LinkShop_Project
    27/02/2025

--- Views/Host.php
+++ Views/Host.php
@@ -4,28 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Host</title>
-    <link rel="stylesheet" href="/Model/home/PageProprerties.css">
-    <link rel="stylesheet" href="/Model/Subscription/hostStyle.css">
-    <link rel="stylesheet" href="/Model/Subscription/hostStyleMain.css">
-    <link rel="stylesheet" href="/Model/Subscription/footer2.css">
-    <script defer src="/Model/Subscription/host.js"></script>
-    <script defer src="/Model/home//home_2ndBar.js"></script>
+    <link rel="stylesheet" href="/Link_Shop_Updated/Model/home/PageProprerties.css">
+    <link rel="stylesheet" href="/Link_Shop_Updated/Model/Subscription/hostStyle.css">
+    <link rel="stylesheet" href="/Link_Shop_Updated/Model/Subscription/hostStyleMain.css">
+    <link rel="stylesheet" href="/Link_Shop_Updated/Model/Subscription/footer2.css">
+    <script defer src="/Link_Shop_Updated/Model/Subscription/host.js"></script>
+    <script defer src="/Link_Shop_Updated/Model/home//home_2ndBar.js"></script>
 </head>
 
 <body>
     <header>
         <div class="contentP" id="content">
             <div class="navBar" id="navBar">
-                <img src="/Model/home/MediaFiles/MozArt blue.png" alt="" class="logotipo">
+                <img src="/Link_Shop_Updated/Model/home/MediaFiles/MozArt blue.png" alt="" class="logotipo">
                 <ul class="navUL" id="navUL">
                     <a href="home.html" class="homeSelected" id="home" onclick="toogle_navClick()">Home</a>
                     <a class="link2" id="menuGeral" onclick="toogle_navClick2()">Menu Geral</a>
-                    <a href="Host.html" id="subescrever" class="link3" onclick="toogle_navClick3()">Subescrever</a>
+                    <a href="Host.php" id="subescrever" class="link3" onclick="toogle_navClick3()">Subescrever</a>
                     <a class="link4" id="contactos" onclick="toogle_contactar()">Contactos</a>
-                    <img src="/Model/home/MediaFiles/SelectionImg.png" alt="" class="imgSelection" id="imgSelection">
+                    <img src="/Link_Shop_Updated/Model/home/MediaFiles/SelectionImg.png" alt="" class="imgSelection" id="imgSelection">
                     <ul class="UlSigns">
-                        <a href="Signs.html" class="link5">SignIn</a>
-                        <a href="Signs.html" class="link6">SignUp</a>
+                        <a href="Signs.php" class="link5">SignIn</a>
+                        <a href="Signs.php" class="link6">SignUp</a>
                     </ul>
                 </ul>
                 <div class="Contact_btn">
@@ -41,15 +41,15 @@
                             <li class="contactos6">873726691</li>
                         </ul>
                         <ul class="imgComunicar">
-                            <img src="/Model/home/MediaFiles/whatsapp.png" alt="" class="imgWhatsapp" id="imgMsg1">
-                            <img src="/Model/home/MediaFiles/facebook (1).png" alt="" class="imgFacebook"
+                            <img src="/Link_Shop_Updated/Model/home/MediaFiles/whatsapp.png" alt="" class="imgWhatsapp" id="imgMsg1">
+                            <img src="/Link_Shop_Updated/Model/home/MediaFiles/facebook (1).png" alt="" class="imgFacebook"
                                 id="imgtendencia">
-                            <img src="/Model/home/MediaFiles/instagram (1).png" alt="" class="imgInstagram"
+                            <img src="/Link_Shop_Updated/Model/home/MediaFiles/instagram (1).png" alt="" class="imgInstagram"
                                 id="imggaleria">
-                            <img src="/Model/home/MediaFiles/twitter.png" alt="" class="imgTwiter" id="c">
-                            <img src="/Model/home/MediaFiles/tik-tok.png" alt="" class="tiktok">
-                            <img src="/Model/home/MediaFiles/chamar.png" alt="" class="imgChamar" id="imgcontactos">
-                            <img src="/Model/home/MediaFiles/chamar.png" alt="" class="imgChamar2">
+                            <img src="/Link_Shop_Updated/Model/home/MediaFiles/twitter.png" alt="" class="imgTwiter" id="c">
+                            <img src="/Link_Shop_Updated/Model/home/MediaFiles/tik-tok.png" alt="" class="tiktok">
+                            <img src="/Link_Shop_Updated/Model/home/MediaFiles/chamar.png" alt="" class="imgChamar" id="imgcontactos">
+                            <img src="/Link_Shop_Updated/Model/home/MediaFiles/chamar.png" alt="" class="imgChamar2">
                         </ul>
                     </div>
                 </div>
@@ -58,7 +58,7 @@
 
         <div class="mainElements">
             <div class="inviteImage">
-                <img src="/Model/Subscription/Icons/DesignInvite.jpg" alt="">
+                <img src="/Link_Shop_Updated/Model/Subscription/Icons/DesignInvite.jpg" alt="">
                 <li class="contraste"></li>
                 <li class="contraste"></li>
 
@@ -79,26 +79,26 @@
             </div>
             <div class="Logos">
                 <div class="logo_Slide" id="logo_Slide">
-                    <img src="/Model/home/MediaFiles/garfo.png" alt="">
-                    <img src="/Model/home/Icons/culinaria.png" alt="">
-                    <img src="/Model/home/MediaFiles/feijoada.png" alt="">
-                    <img src="/Model/Subscription/Icons2/icons8-corel-draw-67.png" alt="">
-                    <img src="/Model/home/MediaFiles/pedaco-de-bolo.png" alt="">
-                    <img src="/Model/Subscription/Icons2/designer-de-web.png" alt="">
-                    <img src="/Model/home/MediaFiles/ui.png" alt="">
-                    <img src="/Model/home/MediaFiles/processador.png" alt="">
-                    <img src="/Model/Subscription/Icons2/adobe-illustrator.png" alt="">
-                    <img src="/Model/Subscription/Icons2/sala-de-estar (1).png" alt="">
-                    <img src="/Model/Subscription/Icons2/photo-camera.png" alt="">
-                    <img src="/Model/Subscription/Icons2/logotipo-do-google-sketchup.png" alt="">
-                    <img src="/Model/Subscription/Icons2/icons8-archicad-24.png" alt="">
-                    <img src="/Model/home/MediaFiles/aprendizagem-online.png" alt="">
-                    <img src="/Model/home/Icons/escrita.png" alt="">
-                    <img src="/Model/Subscription/Icons2/photoshop.png" alt="">
-                    <img src="/Model/home/MediaFiles/colar.png" alt="">
-                    <img src="/Model/home/MediaFiles/iphone.png" alt="">
-                    <img src="/Model/Subscription/Icons2/indesign.png" alt="">
-                    <img src="/Model/Subscription/Icons2/roupas-masculinas.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/home/MediaFiles/garfo.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/home/Icons/culinaria.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/home/MediaFiles/feijoada.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/Subscription/Icons2/icons8-corel-draw-67.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/home/MediaFiles/pedaco-de-bolo.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/Subscription/Icons2/designer-de-web.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/home/MediaFiles/ui.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/home/MediaFiles/processador.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/Subscription/Icons2/adobe-illustrator.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/Subscription/Icons2/sala-de-estar (1).png" alt="">
+                    <img src="/Link_Shop_Updated/Model/Subscription/Icons2/photo-camera.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/Subscription/Icons2/logotipo-do-google-sketchup.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/Subscription/Icons2/icons8-archicad-24.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/home/MediaFiles/aprendizagem-online.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/home/Icons/escrita.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/Subscription/Icons2/photoshop.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/home/MediaFiles/colar.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/home/MediaFiles/iphone.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/Subscription/Icons2/indesign.png" alt="">
+                    <img src="/Link_Shop_Updated/Model/Subscription/Icons2/roupas-masculinas.png" alt="">
                 </div>
             </div>
         </div>
@@ -216,7 +216,7 @@
                 <span class="inputDsc">Insira a tua foto/a do teu negocio</span>
                 <input type="file" class="file_input" id="file">
                 <label for="file" class="labelPpt">Escolher foto</label>
-                <img src="/Model/Subscription/Icons2/upload-na-nuvem.png" alt="" class="imgUpload">
+                <img src="/Link_Shop_Updated/Model/Subscription/Icons2/upload-na-nuvem.png" alt="" class="imgUpload">
             </div>
             <li class="linhaDiv"></li>
 
@@ -240,10 +240,10 @@
                         <option value="">Culinaria</option>
                         <option value="">Venda de diversos</option>
                         <option value="">Artes visuais</option>
-                        <option value="">redacao e escrita</option>
-                        <option value="">Publicidade</option>
-                        <option value="">Tecnologia</option>
-                        <option value="">Beleza e estetica</option>
+                        <option value="">redacao/Link_Shop_Updated e escrita</option>
+                        <option value="">Publici/Link_Shop_Updateddade</option>
+                        <option value="">Tecnolo/Link_Shop_Updatedgia</option>
+                        <option value="">Beleza /Link_Shop_Updatede estetica</option>
                         <option value="">aprendizagem</option>
                         <option value="">Cursos</option>
                     </select>
@@ -293,7 +293,7 @@
                     <span class="inputDsc">Insira a tua foto/a do teu negocio</span>
                     <input type="file" class="file_input" id="file">
                     <label for="file" class="labelPpt">Escolher foto</label>
-                    <img src="/Model/Subscription/Icons2/upload-na-nuvem.png" alt="" class="imgUpload">
+                    <img src="/Link_Shop_Updated/Model/Subscription/Icons2/upload-na-nuvem.png" alt="" class="imgUpload">
                 </div>
 
                 <span class="tipo_publicidade">Escolha o tipo de publicidade</span>
@@ -303,15 +303,15 @@
                 </ul>
                 <ul class="boxes_Radio">
                     <div class="box1">
-                        <li class="verificacao"><img src="/Model/home/MediaFiles/verificado.png" alt=""></li>
-                        <img src="/Model/home/MediaFiles/3d-house-model-with-modern-architecture.jpg" alt=""
+                        <li class="verificacao"><img src="/Link_Shop_Updated/Model/home/MediaFiles/verificado.png" alt=""></li>
+                        <img src="/Link_Shop_Updated/Model/home/MediaFiles/3d-house-model-with-modern-architecture.jpg" alt=""
                             class="imgBoxSelection">
 
                         <li class="boxOverlay"></li>
                     </div>
                     <div class="box2">
                         <li class="verificacao"><img src="/Model/home/MediaFiles/verificado.png" alt=""></li>
-                        <img src="/Model/home/MediaFiles/3d-house-model-with-modern-architecture.jpg" alt=""
+                        <img src="/Link_Shop_Updated/Model/home/MediaFiles/3d-house-model-with-modern-architecture.jpg" alt=""
                             class="imgBoxSelection2">
 
                         <li class="boxOverlay"></li>
@@ -333,15 +333,15 @@
     <footer>
         <div class="footerLine">
             <span class="footerlogo">
-                <img src="/Model/Subscription/Icons2/MozArt blue.png" alt="logotipo MozArt" class="logoMArt">
+                <img src="/Link_Shop_Updated/Model/Subscription/Icons2/MozArt blue.png" alt="logotipo MozArt" class="logoMArt">
                 <h3 class="mzArt">MozArt</h3>
                 <span class="mzArtDsc">Graphic Design Services</span>
             </span>
             <ul class="ImgAdress">
-                <a href=""><img src="/Model/Subscription/Icons2/whatsapp.png" alt="" class="img1"></a>
-                <a href=""><img src="/Model/Subscription/Icons2/facebook (1).png" alt="" class="img2"></a>
-                <a href=""><img src="/Model/Subscription/Icons2/instagram (1).png" alt="" class="img3"></a>
-                <a href=""><img src="/Model/Subscription/Icons2/gmail.png" alt="" class="img4"></a>
+                <a href=""><img src="/Link_Shop_Updated/Model/Subscription/Icons2/whatsapp.png" alt="" class="img1"></a>
+                <a href=""><img src="/Link_Shop_Updated/Model/Subscription/Icons2/facebook (1).png" alt="" class="img2"></a>
+                <a href=""><img src="/Link_Shop_Updated/Model/Subscription/Icons2/instagram (1).png" alt="" class="img3"></a>
+                <a href=""><img src="/Link_Shop_Updated/Model/Subscription/Icons2/gmail.png" alt="" class="img4"></a>
                 <a href=""><img src="" alt=""></a>
             </ul>
             <ul class="contactos2">
